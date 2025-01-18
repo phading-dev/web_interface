a261033bbafb4a4d5402ba876489521e14ad3dbf
Yuxuan Xie <ykuyo.dev@gmail.com>
2025-01-17 17:13:07 -0800

    Expose UrlBuilder in client to avoid dependency on ORIGIN param. Release

--- client.ts
+++ client.ts
@@ -1,8 +1,21 @@
 import { RootPage } from "./interface";
 import { ORIGIN, PARAM_KEY } from "./vars";
 
-export function buildUrl(value: RootPage, origin = ORIGIN): string {
-  let url = new URL(origin);
-  url.searchParams.set(PARAM_KEY, JSON.stringify(value));
-  return url.href;
+export class UrlBuilder {
+  private url: URL;
+
+  public constructor(origin = ORIGIN) {
+    this.url = new URL(origin);
+  }
+
+  public set(value: RootPage): this {
+    this.url.searchParams.set(PARAM_KEY, JSON.stringify(value));
+    return this;
+  }
+
+  public build(): string {
+    return this.url.href;
+  }
 }
+
+export let URL_BUILDER = new UrlBuilder();
