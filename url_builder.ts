62a4cb3062ffbd2d9b7f7dd70bf71496a91ef7c3
Yuxuan Xie <ykuyo.dev@gmail.com>
2025-01-21 20:33:43 -0800

    Added buildSetConnectedAccountOnboardedUrl. Release

--- url_builder.ts
+++ url_builder.ts
@@ -1,9 +1,11 @@
 import { MainApp } from "./main_app";
 import { ReplacePrimaryPaymentMethod } from "./replace_primary_payment_method";
+import { SetConnectedAccountOnboarded } from "./set_connected_account_onboarded";
 import { ORIGIN, PARAM_KEY } from "./vars";
 import {
   MAIN_APP_DEF,
   REPLACE_PRIMARY_PAYMENT_METHOD_DEF,
+  SET_CONNECTED_ACCOUNT_ONBOARDED_DEF,
   WebAppDef,
 } from "./web_apps";
 
@@ -23,16 +25,20 @@ export class UrlBuilder {
 
 export let URL_BUILDER = new UrlBuilder();
 
-export function buildMainAppUrl(builder: UrlBuilder, mainApp: MainApp): string {
-  return builder.build(MAIN_APP_DEF, mainApp);
+export function buildMainAppUrl(builder: UrlBuilder, value: MainApp): string {
+  return builder.build(MAIN_APP_DEF, value);
 }
 
 export function buildReplacePrimaryPaymentMethodUrl(
   builder: UrlBuilder,
-  replacePrimaryPaymentMethod: ReplacePrimaryPaymentMethod,
+  value: ReplacePrimaryPaymentMethod,
 ): string {
-  return builder.build(
-    REPLACE_PRIMARY_PAYMENT_METHOD_DEF,
-    replacePrimaryPaymentMethod,
-  );
+  return builder.build(REPLACE_PRIMARY_PAYMENT_METHOD_DEF, value);
+}
+
+export function buildSetConnectedAccountOnboardedUrl(
+  builder: UrlBuilder,
+  value: SetConnectedAccountOnboarded,
+): string {
+  return builder.build(SET_CONNECTED_ACCOUNT_ONBOARDED_DEF, value);
 }
